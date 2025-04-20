98274dfdbdb95e95593cf77562744323eb10452c
vyacheslav <yachesberg@gmail.com>
2025-04-21 00:52:05 +0300

    refactor: total directory export

---
+++ src/components/shared/index.ts
@@ -0,0 +1,5 @@
+import Header from './header';
+import Container from './container';
+import LocationButton from './location-button';
+
+export { Header, Container, LocationButton };
---
+++ src/components/ui/index.ts
@@ -0,0 +1,13 @@
+export { Badge } from './badge';
+export { Button } from './button';
+export { Checkbox } from './checkbox';
+export { Dialog } from './dialog';
+export { Drawer } from './drawer';
+export { Form } from './form';
+export { Input } from './input';
+export { Label } from './label';
+export { Popover } from './popover';
+export { RadioGroup } from './radio-group';
+export { Skeleton } from './skeleton';
+export { Toaster } from './sonner';
+export { Tooltip } from './tooltip';
